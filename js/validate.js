119fde03c71215c4369ab3ba7ed04a6b6cb2e3a6
vikulchik <kvvendetta@gmail.com>
2019-04-12 19:13:55 +0300

    Add validate for hashtags.

--- js/validate.js
+++ js/validate.js
@@ -2,14 +2,15 @@
 
 (function () {
   var textHashtags = document.querySelector('.text__hashtags');
-  function testField(text) {
-    if (text[0] !== '#') {
-      textHashtags.setCustomValidity('error');
-    }
-  }
+  textHashtags.addEventListener('input', function (evt) {
+    var target = evt.target;
+    var hashtags = target.value.trim().replace(/\s\s+/, ' ').split(' ');
+    for (var i = 0; i < hashtags.length; i++) {
+      if (hashtags[i][0] !== '#') {
+        textHashtags.setCustomValidity('Хэш-тег должен начинается с символа #');
+      }
 
-  document.querySelector('.img-upload__form').addEventListener('submit', function (e) {
-    e.preventDefault();
-    testField(textHashtags.value);
+    }
   });
+
 })();
